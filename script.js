33c203f73d2bfe4a550ceb21b9b1f8668260d7ca
Satish Gautam <150673586+Satish-9567@users.noreply.github.com>
2024-08-20 22:18:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student = {
+  name: "Satish Gautam"
+};
+
+// Add a property to the Object prototype called getKeys()
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Example usage
+console.log(student.getKeys());
